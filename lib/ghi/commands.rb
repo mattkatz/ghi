70d6d66721d85e996369ba49ca5ebb2f99f362d0
mattkatz <github@morelightmorelight.com>
2012-09-10 17:45:44 -0300

    add sync command

--- lib/ghi/commands.rb
+++ lib/ghi/commands.rb
@@ -16,5 +16,6 @@ module GHI
     autoload :Show,      'ghi/commands/show'
     autoload :Unassign,  'ghi/commands/unassign'
     autoload :Version,   'ghi/commands/version'
+    autoload :Sync,      'ghi/commands/sync'
   end
 end
